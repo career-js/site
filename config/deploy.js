ddac10ef71da8f6186af9ff14350aca8a111d3a4
Erik Hanchett <icystorm@gmail.com>
2016-08-03 02:25:35 -0400

    added deploy and changed hash to history

---
+++ config/deploy.js
@@ -0,0 +1,47 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {
+		environment: deployTarget
+	},
+	'revision-data': {
+		type: 'git-commit'
+	},
+	'ssh-index': {
+		remoteDir: "/home/deploy/careerjs",
+		username: process.env['username'],
+		host: process.env['host_ip'],
+		privateKeyFile: process.env['privkey'],
+		allowOverwrite: true
+	},
+	rsync: {
+		dest: "/home/deploy/careerjs",
+		username:   "deploy",
+		host:  "deploy@162.243.254.40", 
+		privateKey:  process.env['privkey'],
+		delete: false
+	}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
+    ENV.locationType = 'history';
     ENV.baseURL = '/site/';
     ENV.googleAnalytics = {
       webPropertyId: 'UA-79722171-1'
