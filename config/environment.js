84a3b6e78500a42dfd02342b540e1b13ed390e13
Erik Hanchett <icystorm@gmail.com>
2016-08-03 23:50:36 -0400

    updated environment

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'none';
-    ENV.baseURL = '/site/';
+    //ENV.baseURL = '/site/';
     ENV.googleAnalytics = {
       webPropertyId: 'UA-79722171-1'
     };
