32e6ea2b8615f2d35ac5aa59fb45b555815ecff3
David Tang <dtang859@gmail.com>
2016-08-27 22:22:10 -0700

    update

--- app/serializers/episode.js
+++ app/serializers/episode.js
@@ -1,6 +1,12 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    payload = Object.keys(payload).map((key) => {
+      return payload[key];
+    });
+    return this._super(store, primaryModelClass, payload, id, requestType);
+  },
   normalize(modelClass, resourceHash, prop) {
     resourceHash.number = resourceHash.number - 1;
     return this._super(...arguments);
